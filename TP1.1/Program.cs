75980370f4c2fc21c775209bdc709cc97f3a05a6
Ailton Costa <166161526+acadl-dev@users.noreply.github.com>
2025-09-13 20:28:39 -0300

    Adicionar arquivos de projeto.

---
+++ TP1.1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace TP1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // decimal valor_desconto = 0.1m;
+           // decimal valor_original = 0.0m;
+
+            CalculateDiscount calculateDiscount;
+            calculateDiscount = CalcularDesconto;
+
+            Console.WriteLine("Insira o valor original do produto:");
+            Console.Write("R$: ");
+            decimal valor_original = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("O valor atualizado é: R$ " + calculateDiscount(valor_original).ToString("F2"));
+            Console.ReadLine(); //Para ler o console
+        }
+
+        public delegate decimal CalculateDiscount(decimal valor_original);
+
+        public static decimal CalcularDesconto(decimal valor_original)
+        {
+            
+            return valor_original - (valor_original * 0.1m);
+
+        }
+    }
+}
---
+++ TP1.2/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace TP1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Pressione 1 + Enter para selecionar seu idioma como Português (BR).");
+            Console.WriteLine("Press 2 + Enter to select your language as English.");
+            Console.WriteLine("Presione 3 + Enter para seleccionar su idioma como Español.");
+            int option = int.Parse(Console.ReadLine());
+            Action<string> escolherIdioma = null; // Declarar o delegate
+
+
+            if (option == 1)
+            {
+                escolherIdioma = idiomaSelecionadoPortugues;
+            }
+            else if (option == 2)
+            {
+                escolherIdioma = idiomaSelecionadoEnglish;
+            }
+            else if (option == 3)
+            {
+                escolherIdioma = idiomaSelecionadoEspanol;
+            }
+            else {
+
+                Console.WriteLine("Opção selecionada inválida: ");
+            }
+
+            // Executar o delegate, se válido
+            if (escolherIdioma != null)
+            {
+                escolherIdioma(""); // Passa string vazia porque o método não usa
+            }
+
+        }
+
+
+
+
+
+        public static void idiomaSelecionadoPortugues(string idioma)
+        {
+            Console.WriteLine("Seja bem vindo(a) ao nosso portal de comunicação!");
+        }
+
+        public static void idiomaSelecionadoEnglish(string idioma)
+        {
+            Console.WriteLine("Welcome to our communication portal!");
+        }
+
+        public static void idiomaSelecionadoEspanol(string idioma)
+        {
+            Console.WriteLine("¡Bienvenido a nuestro portal de comunicación!");
+        }
+    }
+}
