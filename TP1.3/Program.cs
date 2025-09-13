0e9693c806b966346f394943afb131e5550f59b8
Ailton Costa <166161526+acadl-dev@users.noreply.github.com>
2025-09-13 20:55:14 -0300

    Adiconando a atividade 3

---
+++ TP1.3/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace TP1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, double, double> calcularArea = CalcularAreaDoRetangulo;
+
+            Console.WriteLine("Insira a base do retângulo em cm");
+            double baseRetangulo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Agora insira altura do retângulo em cm");
+            double alturaRetangulo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("A área desse retângulo é: " + calcularArea(baseRetangulo, alturaRetangulo) + "cm^2");
+
+        }
+
+        public static double CalcularAreaDoRetangulo(double baseRetangulo, double alturaRetangulo)
+        {
+            return baseRetangulo * alturaRetangulo;
+        }
+    }
+}
